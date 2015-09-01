60766acdb4065c2bb2c5aeb7282e7ed96358f17a
Erik-Anders Nielsen <egnielsen92@gmail.com>
2015-09-01 15:38:30 -0400

    Added Game, starting Eleven Note

---
+++ ElevenNote.Web/Controllers/GameController.cs
@@ -0,0 +1,80 @@
+﻿using ElevenNote.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        #region Get
+        // GET: Game
+        [HttpGet]
+        [ActionName("Index")]
+        public ActionResult IndexGet()
+        {
+            var correctAnswer = new Random().Next(1, 999);
+            var guessCount = 0;
+            Session["Answer"] = correctAnswer;
+            Session["Guess Count"] = guessCount;
+            return View();
+        }
+        
+        #endregion
+
+        #region POST
+
+        // POST: Game
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult IndexPost(GuessingGameViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Session["Guess Count"]=(int)Session["Guess Count"]+1;
+                if (model.Guess == (int)Session["Answer"])
+                {
+                    ViewBag.Win = 0;
+                }
+
+                else
+                {
+                    var myVar = Math.Abs(model.Guess - (int)Session["Answer"]);
+                    if (myVar <= 10)
+                    {
+                        ViewBag.Win = 1;
+                    }
+
+                    else if (myVar <= 50)
+                    {
+                        ViewBag.Win = 2;
+                    }
+
+                    else if (myVar <= 100)
+                    {
+                        ViewBag.Win = 3;
+                    }
+
+                    else if (myVar <= 400)
+                    {
+                        ViewBag.Win = 4;
+                    }
+
+                    else
+                    {
+                        ViewBag.Win = 5;
+                    }
+
+                }
+
+            }
+            return View(model);
+        }
+
+        #endregion
+
+    }
+}
---
+++ ElevenNote.Web/Models/GuessingGameViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.Web.Models
+{
+    public class GuessingGameViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(2, ErrorMessage = "Must contain at least 2 letters")]
+        [MaxLength(20, ErrorMessage = "Must contain no more than 20 letters")]
+        [Display(Name = "Your Name")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "A guess is required")]
+        [Range(1, 999, ErrorMessage = "Guess must be between 1 and 999")]
+        [Display(Name = "Your Guess")]
+        public int Guess { get; set; }
+
+    }
+}
