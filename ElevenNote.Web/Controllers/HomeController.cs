e0f45db421cd21fea38a84f88a18dab9241e49de
Erik-Anders Nielsen <egnielsen92@gmail.com>
2015-08-31 20:07:25 -0400

    Day 1 Work

---
+++ ElevenNote.Web/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult EA()
+        {
+            ViewBag.Message = "I'm a verb now Lana, DEAL WITH IT!";
+
+            return View();
+        }
+
+        public ActionResult Nielsen()
+        {
+            ViewBag.Message = "Just DO IT! Don't let your dreams stay dreams!";
+
+            return View();
+        }
+    }
+}
